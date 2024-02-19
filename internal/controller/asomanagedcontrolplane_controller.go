0a19f608c3b9caf761ca67e1a2d12617638d407a
Jon Huhn <huhnjon@gmail.com>
2024-02-19 14:18:57 -0600

    reconcile kubeconfig in ASOManagedControlPlane

--- internal/controller/asomanagedcontrolplane_controller.go
+++ internal/controller/asomanagedcontrolplane_controller.go
@@ -20,6 +20,11 @@ import (
 	"context"
 	"fmt"
 
+	asocontainerservicev1 "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20231001"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/utils/ptr"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
@@ -27,6 +32,7 @@ import (
 	"sigs.k8s.io/cluster-api/util"
 	"sigs.k8s.io/cluster-api/util/patch"
 	"sigs.k8s.io/cluster-api/util/predicates"
+	"sigs.k8s.io/cluster-api/util/secret"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -116,17 +122,81 @@ func (r *ASOManagedControlPlaneReconciler) reconcileNormal(ctx context.Context, 
 	if err != nil {
 		return ctrl.Result{}, err
 	}
-
-	asoControlPlane.Status.Ready = true
+	infraReady := true
 	for _, status := range asoControlPlane.GetResourceStatuses() {
 		if !ptr.Deref(status.Ready, true) {
-			asoControlPlane.Status.Ready = false
+			infraReady = false
 			break
 		}
 	}
+
+	if infraReady {
+		err := r.reconcileKubeconfig(ctx, asoControlPlane, cluster)
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to reconcile kubeconfig: %w", err)
+		}
+	}
+
+	asoControlPlane.Status.Ready = infraReady
 	asoControlPlane.Status.Initialized = asoControlPlane.Status.Ready
 
 	return ctrl.Result{}, nil
+}
+
+func (r *ASOManagedControlPlaneReconciler) reconcileKubeconfig(ctx context.Context, asoControlPlane *infrav1.ASOManagedControlPlane, cluster *clusterv1.Cluster) error {
+	var umc *unstructured.Unstructured
+	for _, resource := range asoControlPlane.Spec.Resources {
+		u := &unstructured.Unstructured{}
+		if err := u.UnmarshalJSON(resource.Raw); err != nil {
+			return fmt.Errorf("failed to unmarshal resource JSON: %w", err)
+		}
+		if u.GroupVersionKind().Group == asocontainerservicev1.GroupVersion.Group &&
+			u.GroupVersionKind().Kind == "ManagedCluster" {
+			umc = u
+			break
+		}
+	}
+	if umc == nil {
+		return fmt.Errorf("no %s ManagedCluster defined in ASOManagedControlPlane spec.resources", asocontainerservicev1.GroupVersion.Group)
+	}
+	// get a typed resource so we don't have to try to convert this unstructured ourselves since it might be a
+	// different API version than we know how to deal with.
+	mc := &asocontainerservicev1.ManagedCluster{}
+	err := r.Get(ctx, client.ObjectKeyFromObject(umc), mc)
+	if err != nil {
+		return fmt.Errorf("error getting ManagedCluster: %w", err)
+	}
+
+	var secretRef *genruntime.SecretDestination
+	if mc.Spec.OperatorSpec != nil &&
+		mc.Spec.OperatorSpec.Secrets != nil {
+		secretRef = mc.Spec.OperatorSpec.Secrets.UserCredentials
+		if mc.Spec.OperatorSpec.Secrets.AdminCredentials != nil {
+			secretRef = mc.Spec.OperatorSpec.Secrets.AdminCredentials
+		}
+	}
+	if secretRef == nil {
+		return fmt.Errorf("ManagedCluster must define at least one of spec.operatorSpec.secrets.{userCredentials,adminCredentials}")
+	}
+	asoKubeconfig := &corev1.Secret{}
+	err = r.Get(ctx, client.ObjectKey{Namespace: cluster.Namespace, Name: secretRef.Name}, asoKubeconfig)
+	if err != nil {
+		return fmt.Errorf("failed to fetch secret created by ASO: %w", err)
+	}
+
+	expectedSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secret.Name(cluster.Name, secret.Kubeconfig),
+			Namespace: cluster.Namespace,
+			// Do we have to/should we set an owner reference to this ASOManagedControlPlane?
+			Labels: map[string]string{clusterv1.ClusterNameLabel: cluster.Name},
+		},
+		Data: map[string][]byte{
+			secret.KubeconfigDataName: asoKubeconfig.Data[secret.KubeconfigDataName],
+		},
+	}
+
+	return r.Patch(ctx, expectedSecret, client.Apply, client.FieldOwner("capaso"), client.ForceOwnership)
 }
 
 func (r *ASOManagedControlPlaneReconciler) reconcileDelete(ctx context.Context, asoControlPlane *infrav1.ASOManagedControlPlane, cluster *clusterv1.Cluster) (ctrl.Result, error) {
@@ -161,6 +231,7 @@ func (r *ASOManagedControlPlaneReconciler) SetupWithManager(ctx context.Context,
 				predicates.ClusterUnpausedAndInfrastructureReady(log.FromContext(ctx)),
 			),
 		).
+		// TODO: watch kubeconfig. does ASO set an controller owner reference on secrets it creates?
 		Build(r)
 	if err != nil {
 		return err
