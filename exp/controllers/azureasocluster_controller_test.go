85be94bafa7ce07bce77770ca165fa60e56dba5d
Jon Huhn <nojnhuh@users.noreply.github.com>
2025-04-08 16:02:54 -0500

    Implement top-level Reconcile method for AzureASOCluster

--- exp/controllers/azureasocluster_controller_test.go
+++ exp/controllers/azureasocluster_controller_test.go
@@ -18,11 +18,15 @@ package controllers
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	infrav1alphaexp "sigs.k8s.io/cluster-api-provider-azure/exp/api/v1alpha1"
@@ -34,6 +38,7 @@ func TestAzureASOClusterReconcile(t *testing.T) {
 	s := runtime.NewScheme()
 	sb := runtime.NewSchemeBuilder(
 		infrav1alphaexp.AddToScheme,
+		clusterv1.AddToScheme,
 	)
 	NewGomegaWithT(t).Expect(sb.AddToScheme(s)).To(Succeed())
 
@@ -52,6 +57,112 @@ func TestAzureASOClusterReconcile(t *testing.T) {
 			Client: c,
 		}
 		result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{Namespace: "doesn't", Name: "exist"}})
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(result).To(Equal(ctrl.Result{}))
+	})
+
+	t.Run("Cluster does not exist", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+
+		asoCluster := &infrav1alphaexp.AzureASOCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "aso-cluster",
+				Namespace: "ns",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: clusterv1.GroupVersion.Identifier(),
+						Kind:       "Cluster",
+						Name:       "cluster",
+					},
+				},
+			},
+		}
+		c := fakeClientBuilder().
+			WithObjects(asoCluster).
+			Build()
+		r := &AzureASOClusterReconciler{
+			Client: c,
+		}
+		_, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(asoCluster)})
+		g.Expect(err).To(HaveOccurred())
+	})
+
+	t.Run("successfully reconciles normally", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+
+		asoCluster := &infrav1alphaexp.AzureASOCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "aso-cluster",
+				Namespace: "ns",
+			},
+		}
+		c := fakeClientBuilder().
+			WithObjects(asoCluster).
+			Build()
+		r := &AzureASOClusterReconciler{
+			Client: c,
+		}
+		result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(asoCluster)})
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(result).To(Equal(ctrl.Result{}))
+	})
+
+	t.Run("successfully reconciles pause", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+
+		cluster := &clusterv1.Cluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "cluster",
+				Namespace: "ns",
+			},
+			Spec: clusterv1.ClusterSpec{
+				Paused: true,
+			},
+		}
+		asoCluster := &infrav1alphaexp.AzureASOCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "aso-cluster",
+				Namespace: cluster.Namespace,
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: clusterv1.GroupVersion.Identifier(),
+						Kind:       "Cluster",
+						Name:       cluster.Name,
+					},
+				},
+			},
+		}
+		c := fakeClientBuilder().
+			WithObjects(cluster, asoCluster).
+			Build()
+		r := &AzureASOClusterReconciler{
+			Client: c,
+		}
+		result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(asoCluster)})
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(result).To(Equal(ctrl.Result{}))
+	})
+
+	t.Run("successfully reconciles delete", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+
+		asoCluster := &infrav1alphaexp.AzureASOCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "aso-cluster",
+				Namespace: "ns",
+				Finalizers: []string{
+					clusterv1.ClusterFinalizer,
+				},
+				DeletionTimestamp: &metav1.Time{Time: time.Date(1, 0, 0, 0, 0, 0, 0, time.UTC)},
+			},
+		}
+		c := fakeClientBuilder().
+			WithObjects(asoCluster).
+			Build()
+		r := &AzureASOClusterReconciler{
+			Client: c,
+		}
+		result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(asoCluster)})
 		g.Expect(err).NotTo(HaveOccurred())
 		g.Expect(result).To(Equal(ctrl.Result{}))
 	})
