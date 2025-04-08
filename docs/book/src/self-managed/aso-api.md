6d81a5c70afff726c5a2dde53499c30f1c41b52c
Jon Huhn <nojnhuh@users.noreply.github.com>
2025-04-08 15:26:31 -0500

    Add boilerplate for AzureASOCluster

---
+++ docs/book/src/self-managed/aso-api.md
@@ -0,0 +1,6 @@
+# ASO API
+
+This document describes how to define self-managed clusters using the ASO API. The `ASOSelfManaged` feature
+gate must be enabled to use these APIs (`EXP_ASO_SELF_MANAGED=true clusterctl init -i azure ...`).
+
+## AzureASOCluster
---
+++ exp/api/v1alpha1/azureasoclustertemplate_webhook_test.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
+)
+
+func TestAzureASOClusterTemplateWebhookValidateCreate(t *testing.T) {
+	tests := []struct {
+		name               string
+		asoClusterTemplate *AzureASOClusterTemplate
+		matchErrs          types.GomegaMatcher
+	}{
+		{
+			name:               "empty",
+			asoClusterTemplate: &AzureASOClusterTemplate{},
+			matchErrs:          Not(HaveOccurred()),
+		},
+	}
+
+	ctx := context.Background()
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			g := NewGomegaWithT(t)
+			a := &azureASOClusterTemplateWebhook{}
+
+			if test.matchErrs == nil {
+				test.matchErrs = BeEmpty()
+			}
+
+			actualWarnings, actualErrs := a.ValidateCreate(ctx, test.asoClusterTemplate)
+			g.Expect(actualWarnings).To(BeEmpty())
+			g.Expect(actualErrs).To(test.matchErrs)
+		})
+	}
+}
