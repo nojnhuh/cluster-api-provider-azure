8f7d3a93af5a2f1c908a6bb7869dcf5029744229
Jon Huhn <nojnhuh@users.noreply.github.com>
2025-04-10 20:16:41 -0500

    Implement top-level Reconcile method for AzureASOMachine

--- exp/controllers/azureasomachine_controller_test.go
+++ exp/controllers/azureasomachine_controller_test.go
@@ -19,12 +19,15 @@ package controllers
 import (
 	"context"
 	"testing"
+	"time"
 
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	infrav1alphaexp "sigs.k8s.io/cluster-api-provider-azure/exp/api/v1alpha1"
@@ -57,5 +60,160 @@ func TestAzureASOMachineReconcile(t *testing.T) {
 		result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{Namespace: "doesn't", Name: "exist"}})
 		g.Expect(err).NotTo(HaveOccurred())
 		g.Expect(result).To(Equal(ctrl.Result{}))
+	})
+
+	t.Run("Machine does not exist", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+
+		asoMachine := &infrav1alphaexp.AzureASOMachine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "aso-machine",
+				Namespace: "ns",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: clusterv1.GroupVersion.Identifier(),
+						Kind:       "Machine",
+						Name:       "machine",
+					},
+				},
+			},
+		}
+		c := fakeClientBuilder().
+			WithObjects(asoMachine).
+			Build()
+		r := &AzureASOMachineReconciler{
+			Client: c,
+		}
+		_, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(asoMachine)})
+		g.Expect(err).To(HaveOccurred())
+	})
+
+	t.Run("successfully reconciles normally", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+
+		cluster := &clusterv1.Cluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "cluster",
+				Namespace: "ns",
+			},
+		}
+		machine := &clusterv1.Machine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "machine",
+				Namespace: cluster.Namespace,
+			},
+		}
+		asoMachine := &infrav1alphaexp.AzureASOMachine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "aso-machine",
+				Namespace: machine.Namespace,
+				Labels: map[string]string{
+					clusterv1.ClusterNameLabel: cluster.Name,
+				},
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: clusterv1.GroupVersion.Identifier(),
+						Kind:       "Machine",
+						Name:       machine.Name,
+					},
+				},
+			},
+		}
+		c := fakeClientBuilder().
+			WithObjects(cluster, machine, asoMachine).
+			Build()
+		r := &AzureASOMachineReconciler{
+			Client: c,
+		}
+		result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(asoMachine)})
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(result).To(Equal((ctrl.Result{})))
+	})
+
+	t.Run("successfully reconciles pause", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+
+		cluster := &clusterv1.Cluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "cluster",
+				Namespace: "ns",
+			},
+			Spec: clusterv1.ClusterSpec{
+				Paused: true,
+			},
+		}
+		machine := &clusterv1.Machine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "machine",
+				Namespace: cluster.Namespace,
+			},
+			Spec: clusterv1.MachineSpec{
+				ClusterName: cluster.Name,
+			},
+		}
+		asoMachine := &infrav1alphaexp.AzureASOMachine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "aso-machine",
+				Namespace: machine.Namespace,
+				Labels: map[string]string{
+					clusterv1.ClusterNameLabel: cluster.Name,
+				},
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: clusterv1.GroupVersion.Identifier(),
+						Kind:       "Machine",
+						Name:       machine.Name,
+					},
+				},
+			},
+		}
+		c := fakeClientBuilder().
+			WithObjects(cluster, machine, asoMachine).
+			Build()
+		r := &AzureASOMachineReconciler{
+			Client: c,
+		}
+		result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(asoMachine)})
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(result).To(Equal((ctrl.Result{})))
+	})
+
+	t.Run("successfully reconciles delete", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+
+		cluster := &clusterv1.Cluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "cluster",
+				Namespace: "ns",
+			},
+		}
+		machine := &clusterv1.Machine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "machine",
+				Namespace: cluster.Namespace,
+			},
+		}
+		asoMachine := &infrav1alphaexp.AzureASOMachine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "aso-machine",
+				Namespace: cluster.Namespace,
+				Labels: map[string]string{
+					clusterv1.ClusterNameLabel: cluster.Name,
+				},
+				Finalizers: []string{
+					infrav1alphaexp.AzureASOMachineFinalizer,
+				},
+				DeletionTimestamp: &metav1.Time{Time: time.Date(1, 0, 0, 0, 0, 0, 0, time.UTC)},
+			},
+		}
+		c := fakeClientBuilder().
+			WithObjects(cluster, machine, asoMachine).
+			Build()
+		r := &AzureASOMachineReconciler{
+			Client: c,
+		}
+		result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(asoMachine)})
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(result).To(Equal((ctrl.Result{})))
 	})
 }
