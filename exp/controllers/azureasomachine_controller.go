eac8375765a44f9d48c70f8216503fbc95b882ee
Jon Huhn <nojnhuh@users.noreply.github.com>
2025-04-10 20:16:41 -0500

    Implement top-level Reconcile method for AzureASOMachine

--- exp/controllers/azureasomachine_controller.go
+++ exp/controllers/azureasomachine_controller.go
@@ -18,11 +18,19 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	"k8s.io/utils/ptr"
+	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/cluster-api/util/annotations"
+	"sigs.k8s.io/cluster-api/util/patch"
 	"sigs.k8s.io/cluster-api/util/predicates"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 
 	infrav1alphaexp "sigs.k8s.io/cluster-api-provider-azure/exp/api/v1alpha1"
 	"sigs.k8s.io/cluster-api-provider-azure/util/tele"
@@ -42,10 +50,34 @@ func (r *AzureASOMachineReconciler) SetupWithManager(ctx context.Context, mgr ct
 	)
 	defer done()
 
-	_, err := ctrl.NewControllerManagedBy(mgr).
+	clusterMapper, err := util.ClusterToTypedObjectsMapper(r.Client, &infrav1alphaexp.AzureASOMachineList{}, mgr.GetScheme())
+	if err != nil {
+		return fmt.Errorf("failed to create mapper for Cluster to AzureASOMachines: %w", err)
+	}
+
+	_, err = ctrl.NewControllerManagedBy(mgr).
 		WithOptions(options).
 		For(&infrav1alphaexp.AzureASOMachine{}).
 		WithEventFilter(predicates.ResourceHasFilterLabel(mgr.GetScheme(), log, r.WatchFilterValue)).
+		WithEventFilter(predicates.ResourceIsNotExternallyManaged(mgr.GetScheme(), log)).
+		Watches(
+			&clusterv1.Machine{},
+			handler.EnqueueRequestsFromMapFunc(
+				util.MachineToInfrastructureMapFunc(infrav1alphaexp.GroupVersion.WithKind(infrav1alphaexp.AzureASOMachineKind)),
+			),
+			builder.WithPredicates(
+				predicates.ResourceHasFilterLabel(mgr.GetScheme(), log, r.WatchFilterValue),
+			),
+		).
+		// Watch clusters for pause/unpause notifications
+		Watches(
+			&clusterv1.Cluster{},
+			handler.EnqueueRequestsFromMapFunc(clusterMapper),
+			builder.WithPredicates(
+				predicates.ResourceHasFilterLabel(mgr.GetScheme(), log, r.WatchFilterValue),
+				predicates.ClusterPausedTransitionsOrInfrastructureProvisioned(mgr.GetScheme(), log),
+			),
+		).
 		Build(r)
 	if err != nil {
 		return err
@@ -59,6 +91,90 @@ func (r *AzureASOMachineReconciler) SetupWithManager(ctx context.Context, mgr ct
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=azureasomachines/finalizers,verbs=update
 
 // Reconcile reconciles an AzureASOMachine.
-func (r *AzureASOMachineReconciler) Reconcile(_ context.Context, _ ctrl.Request) (result ctrl.Result, resultErr error) {
+func (r *AzureASOMachineReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, resultErr error) {
+	ctx, _, done := tele.StartSpanWithLogger(ctx,
+		"controllers.AzureASOMachineReconciler.Reconcile",
+		tele.KVP("namespace", req.Namespace),
+		tele.KVP("name", req.Name),
+		tele.KVP("kind", infrav1alphaexp.AzureASOMachineKind),
+	)
+	defer done()
+
+	asoMachine := &infrav1alphaexp.AzureASOMachine{}
+	err := r.Get(ctx, req.NamespacedName, asoMachine)
+	if err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	patchHelper, err := patch.NewHelper(asoMachine, r.Client)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to create patch helper: %w", err)
+	}
+	defer func() {
+		err := patchHelper.Patch(ctx, asoMachine)
+		if err != nil && resultErr == nil {
+			resultErr = err
+			result = ctrl.Result{}
+		}
+	}()
+
+	machine, err := util.GetOwnerMachine(ctx, r.Client, asoMachine.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	cluster, err := util.GetClusterFromMetadata(ctx, r.Client, asoMachine.ObjectMeta)
+	if client.IgnoreNotFound(err) != nil {
+		return ctrl.Result{}, err
+	}
+
+	if cluster != nil && ptr.Deref(cluster.Spec.Paused, false) ||
+		annotations.HasPaused(asoMachine) {
+		return r.reconcilePaused(ctx, asoMachine)
+	}
+
+	if !asoMachine.GetDeletionTimestamp().IsZero() {
+		return r.reconcileDelete(ctx, asoMachine)
+	}
+
+	return r.reconcileNormal(ctx, asoMachine, machine)
+}
+
+func (r *AzureASOMachineReconciler) reconcileNormal(ctx context.Context, _ *infrav1alphaexp.AzureASOMachine, _ *clusterv1.Machine) (ctrl.Result, error) {
+	ctx, log, done := tele.StartSpanWithLogger(ctx,
+		"controllers.AzureASOMachineReconciler.reconcileNormal",
+	)
+	defer done()
+	log.V(4).Info("reconciling normally")
+
+	// this will be used soon
+	_ = ctx
+
+	return ctrl.Result{}, nil
+}
+
+func (r *AzureASOMachineReconciler) reconcilePaused(ctx context.Context, _ *infrav1alphaexp.AzureASOMachine) (ctrl.Result, error) {
+	ctx, log, done := tele.StartSpanWithLogger(ctx,
+		"controllers.AzureASOMachineReconciler.reconcilePaused",
+	)
+	defer done()
+	log.V(4).Info("reconciling pause")
+
+	// this will be used soon
+	_ = ctx
+
+	return ctrl.Result{}, nil
+}
+
+func (r *AzureASOMachineReconciler) reconcileDelete(ctx context.Context, _ *infrav1alphaexp.AzureASOMachine) (ctrl.Result, error) {
+	ctx, log, done := tele.StartSpanWithLogger(ctx,
+		"controllers.AzureASOMachineReconciler.reconcileDelete",
+	)
+	defer done()
+	log.V(4).Info("reconciling delete")
+
+	// this will be used soon
+	_ = ctx
+
 	return ctrl.Result{}, nil
 }
