ad2f17123c69905d40d5945b2ebdccf503387c6c
Jon Huhn <nojnhuh@users.noreply.github.com>
2025-04-19 19:01:56 -0500

    Export ResourceReconciler fields

--- controllers/azureasomanagedcontrolplane_controller.go
+++ controllers/azureasomanagedcontrolplane_controller.go
@@ -102,9 +102,9 @@ func (r *AzureASOManagedControlPlaneReconciler) SetupWithManager(ctx context.Con
 	r.newResourceReconciler = func(asoManagedCluster *infrav1.AzureASOManagedControlPlane, resources []*unstructured.Unstructured) resourceReconciler {
 		return &ResourceReconciler{
 			Client:    r.Client,
-			resources: resources,
-			owner:     asoManagedCluster,
-			watcher:   externalTracker,
+			Resources: resources,
+			Owner:     asoManagedCluster,
+			Watcher:   externalTracker,
 		}
 	}
 
